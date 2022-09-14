6adcc4fb4c16d97a63d078047692cb6f7eb4549f
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-14 16:14:31 -0300

    feat - phishing edited

--- src/components/Dialogs/InitialWarning.jsx
+++ src/components/Dialogs/InitialWarning.jsx
@@ -1,21 +1,18 @@
 import "./InitialWarning.css";
 
+import { BsFillExclamationTriangleFill } from "react-icons/bs";
 import React from "react";
 
 function InitialWarning({ onClick }) {
   return (
     <section className="warning_container">
       <div>
+        <BsFillExclamationTriangleFill color="red" size={70} />
         <h1>¡Atencion!</h1>
         <p>
-          Este sitio fue creado con fines educativos, no es oficial de Mercado
-          Libre. Puedes ver/utilizar el repositorio libremente, lo encontraras
-          en mi{" "}
-          <a
-            href="https://github.com/franRappazzini"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          Este sitio fue creado con fines educativos, no es oficial de Mercado Libre. Puedes
+          ver/utilizar el repositorio libremente, lo encontraras en mi{" "}
+          <a href="https://github.com/franRappazzini" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
           . Gracias!
